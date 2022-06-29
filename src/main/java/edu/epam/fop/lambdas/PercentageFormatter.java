88caad8a7f5daddae0a9adc829a18bfb506518a5
Vadim Markov <vadim_markov@epam.com>
2022-06-30 01:15:06 +0200

    Initial

---
+++ src/main/java/edu/epam/fop/lambdas/PercentageFormatter.java
@@ -0,0 +1,9 @@
+package edu.epam.fop.lambdas;
+
+import java.util.function.DoubleFunction;
+
+public interface PercentageFormatter {
+
+  // TODO write you code here
+  DoubleFunction<String> INSTANCE = null;
+}
