6147f1357df5bfaf6c9920101a93e3170419c28c
Prasad52892 <prasadbonela52892@gmail.com>
2024-07-28 14:26:52 +0530

    Solved

--- src/main/java/edu/epam/fop/lambdas/PercentageFormatter.java
+++ src/main/java/edu/epam/fop/lambdas/PercentageFormatter.java
@@ -4,6 +4,24 @@ import java.util.function.DoubleFunction;
 
 public interface PercentageFormatter {
 
-  // TODO write you code here
-  DoubleFunction<String> INSTANCE = null;
+  DoubleFunction<String> INSTANCE = value -> {
+    if (value > 1) {
+      value *= 100;
+      double fraction_part = value - (int) value;
+
+      if (fraction_part < 0.5) {
+        return (int) Math.floor(value) + " %";
+      } else {
+        return (int) Math.ceil(value) + " %";
+      }
+    } else {
+      value *= 100;
+      double fraction_part = value - (int) value;
+      if (fraction_part == 0) {
+        return (int) value + " %";
+      } else {
+        return String.format("%.1f", value).replace("," , ".") + " %";
+      }
+    }
+  };
 }
